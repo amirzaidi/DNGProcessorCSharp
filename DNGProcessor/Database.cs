24d3b78c7c7441a88e276e43b7383d6b3c6d6991
Amir Zaidi <azaidi@live.nl>
2018-07-15 20:33:04 +0200

    Continue work on parsing tags

---
+++ DNGProcessor/Database.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+namespace DNGProcessor
+{
+    enum TIFFTag
+    {
+        NewSubfileType = 254,
+        ImageWidth = 256,
+        ImageLength = 257,
+        BitsPerSample = 258,
+        Compression = 259,
+        PhotometricInterpretation = 262,
+        ImageDescription = 270,
+        Make = 271,
+        Model = 272,
+        StripOffsets = 273,
+        Orientation = 274,
+        SamplesPerPixel = 277,
+        RowsPerStrip = 278,
+        StripByteCounts = 279,
+        XResolution = 282,
+        YResolution = 283,
+        PlanarConfiguration = 284,
+        ResolutionUnit = 296,
+        Software = 305,
+        Hardware = 306,
+        CFARepeatPatternDim = 33421,
+        CFAPattern = 33422,
+        Copyright = 33432,
+        ExposureTime = 33434,
+        FNumber = 33437,
+        ISOSpeedRatings = 34855,
+        DateTimeOriginal = 36867,
+        FocalLength = 37386,
+        EPStandardID = 37398,
+        DNGVersion = 50706,
+        DNGBackwardVersion = 50707,
+        UniqueCameraModel = 50708,
+        CFAPlaneColor = 50710,
+        CFALayout = 50711,
+        BlackLevelRepeatDim = 50713,
+        BlackLevel = 50714,
+        WhiteLevel = 50717,
+        DefaultScale = 50718,
+        DefaultCropOrigin = 50719,
+        DefaultCropSize = 50720,
+        ColorMatrix1 = 50721,
+        ColorMatrix2 = 50722,
+        CameraCalibration1 = 50723,
+        CameraCalibration2 = 50724,
+        AsShotNeutral = 50728,
+        CalibrationIlluminant1 = 50778,
+        CalibrationIlluminant2 = 50779,
+        ActiveArea = 50829,
+        ForwardMatrix1 = 50964,
+        ForwardMatrix2 = 50965,
+        OpcodeList2 = 51009,
+        OpcodeList3 = 51022,
+        NoiseProfile = 51041
+    }
+
+    enum TIFFTagValueType
+    {
+        Byte = 1,
+        String = 2,
+        Int16 = 3,
+        Int32 = 4,
+        Frac = 5,
+        Undef = 7,
+        SFrac = 10,
+        Double = 12
+    }
+
+    struct TagData
+    {
+        internal TIFFTagValueType Type;
+        internal object[] Data;
+
+        internal TagData(TIFFTagValueType Type, object[] Data)
+        {
+            this.Type = Type;
+            this.Data = Data;
+        }
+    }
+
+    partial class Program
+    {
+        static Dictionary<TIFFTagValueType, int> unitLengths = new Dictionary<TIFFTagValueType, int>()
+        {
+            { TIFFTagValueType.Byte, 1 },
+            { TIFFTagValueType.String, 1 },
+            { TIFFTagValueType.Int16, 2 },
+            { TIFFTagValueType.Int32, 4 },
+            { TIFFTagValueType.Frac, 8 },
+            { TIFFTagValueType.Undef, 0 },
+            { TIFFTagValueType.SFrac, 8 },
+            { TIFFTagValueType.Double, 8 },
+        };
+    }
+}
---
+++ DNGProcessor/Fraction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DNGProcessor
+{
+    class Fraction
+    {
+        private long mNumerator, mDenominator;
+
+        public Fraction(long num, long den)
+        {
+            long gcd = GCD(Math.Abs(num), Math.Abs(den));
+            mNumerator = num / gcd;
+            mDenominator = den / gcd;
+        }
+
+        public override string ToString()
+        {
+            return mNumerator.ToString() + '/' + mDenominator.ToString();
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.mNumerator * b.mDenominator + b.mNumerator * a.mDenominator, 
+                a.mDenominator * b.mDenominator);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.mNumerator * b.mDenominator - b.mNumerator * a.mDenominator,
+                a.mDenominator * b.mDenominator);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.mNumerator * b.mNumerator, a.mDenominator * b.mDenominator);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.mNumerator * b.mDenominator, a.mDenominator * b.mNumerator);
+        }
+
+        public static implicit operator double(Fraction f)
+        {
+            return (double)f.mNumerator / f.mDenominator;
+        }
+
+        private static long GCD(long a, long b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
